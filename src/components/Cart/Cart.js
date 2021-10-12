b5a91a88865222c4c72b9502dd56a19a5a91e3b8
Akash Islam <akzzislam19194@gmail.com>
2021-10-13 02:37:58 +0600

    first commit

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './Cart.css'
+
+const Cart = (props) => {
+    const { cart } = props;
+    let totalQuantity = 0;
+    let total = 0;
+    for (const product of cart) {
+        product.quantity = !product.quantity ? 1 : product.quantity;
+        total = total + product.price * product.quantity;
+        totalQuantity = totalQuantity + product.quantity;
+    }
+    const shipping = total > 0 ? 15 : 0;
+    const tax = (total + shipping) * .10;
+    const grandTotal = total + tax + shipping;
+
+    return (
+        <div className="cart">
+            <h3>Order Summary</h3>
+            <h3>Items Orders : {totalQuantity}</h3>
+            <br />
+            <p>Total {total.toFixed(2)}</p>
+            <p>Shipping : {shipping}</p>
+            <p>Tax : {tax.toFixed(2)}</p>
+            <p>Grand Total : {grandTotal.toFixed(2)}</p>
+        </div>
+    );
+};
+
+export default Cart;
---
+++ src/components/Product/Product.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './Product.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import Rating from 'react-rating';
+
+const Product = (props) => {
+    // console.log(props);
+    const { name, img, seller, price, stock, star } = props.product;
+    const cartIcon = <FontAwesomeIcon icon={faShoppingCart} />
+    return (
+        <div className="product">
+            <div>
+                <img src={img} alt="" />
+            </div>
+            <div>
+                <h3 className="product-name">Product Name: {name}</h3>
+                <p>By : {seller}</p>
+                <h4>Price : {price}</h4>
+                <Rating
+                    initialRating={star}
+                    emptySymbol="far fa-star icon-colour"
+                    fullSymbol="fas fa-star icon-colour"
+                    readonly></Rating>
+                <h5>only {stock} left in stock - order soon</h5>
+                <button onClick={() => props.handleAddToCart(props.product)}
+                    className="btn-regular">{cartIcon} Add to Cart</button>
+            </div>
+        </div>
+    );
+};
+
+export default Product;
